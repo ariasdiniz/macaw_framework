48715a21f82f10dc63e6f20d69cd38c5a1ed24cb
Aria Diniz <aria.diniz.dev@gmail.com>
2023-04-26 07:28:01 -0300

    Making Cache Aspect and Middleware thread safe
    - In order to ensure thread safety, Mutex
    synchronization was introduced in both
    the aspect and the middleware

--- lib/macaw_framework/aspects/cache_aspect.rb
+++ lib/macaw_framework/aspects/cache_aspect.rb
@@ -5,10 +5,13 @@
 module CacheAspect
   def call_endpoint(cache, *args)
     return super(*args) unless !cache[:cache].nil? && cache[:endpoints_to_cache].include?(args[0])
-    return cache[:cache].cache[args[1..].to_s.to_sym][0] unless cache[:cache].cache[args[1..].to_s.to_sym].nil?
 
-    response = super(*args)
-    cache[:cache].cache[args[1..].to_s.to_sym] = [response, Time.now]
-    response
+    cache[:cache].mutex.synchronize do
+      return cache[:cache].cache[args[1..].to_s.to_sym][0] unless cache[:cache].cache[args[1..].to_s.to_sym].nil?
+
+      response = super(*args)
+      cache[:cache].cache[args[1..].to_s.to_sym] = [response, Time.now]
+      response
+    end
   end
 end
