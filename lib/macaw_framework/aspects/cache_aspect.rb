4d72f21b85c8adbef22fd9f963de0b699bd24681
Aria Diniz <aria.diniz.dev@gmail.com>
2023-04-27 20:44:38 -0300

    Improving caching strategy
    - Add a new configuration option in the cache section in
      the application.json file, called ignore_headers which
      will accept an array of header names that should not be
      considered when determining cache usage

--- lib/macaw_framework/aspects/cache_aspect.rb
+++ lib/macaw_framework/aspects/cache_aspect.rb
@@ -6,12 +6,21 @@ module CacheAspect
   def call_endpoint(cache, *args)
     return super(*args) unless !cache[:cache].nil? && cache[:endpoints_to_cache].include?(args[0])
 
+    cache_filtered_name = cache_name_filter(args[1], cache[:ignored_headers])
+
     cache[:cache].mutex.synchronize do
-      return cache[:cache].cache[args[1..].to_s.to_sym][0] unless cache[:cache].cache[args[1..].to_s.to_sym].nil?
+      return cache[:cache].cache[cache_filtered_name][0] unless cache[:cache].cache[cache_filtered_name].nil?
 
       response = super(*args)
-      cache[:cache].cache[args[1..].to_s.to_sym] = [response, Time.now]
+      cache[:cache].cache[cache_filtered_name] = [response, Time.now]
       response
     end
+  end
+
+  private
+
+  def cache_name_filter(client_data, ignored_headers)
+    filtered_headers = client_data[:headers].filter { |key, _value| !ignored_headers.include?(key) }
+    [{ body: client_data[:body], params: client_data[:params], headers: filtered_headers }].to_s.to_sym
   end
 end
