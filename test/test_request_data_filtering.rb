04e3541b589613138b2d5f8497be69682dd3efc0
Aria Diniz <aria.diniz.dev@gmail.com>
2023-05-06 09:47:32 -0300

    Creating log sanitization to prevent log forging
    - In order to filter logs to prevent log forging
    attacks and to make them more legible, the
    module LogDataFilter was created. It will sanitize
    every log registered by the logging aspect.
    
    - Also, more tests were created for Macaw class
    and Request Data Filter module.

--- test/test_request_data_filtering.rb
+++ test/test_request_data_filtering.rb
@@ -92,4 +92,50 @@ class TestRequestDataFiltering < Minitest::Test
     client_data = File.open("./test/data/client_data_path_variable.txt")
     assert_raises(EndpointNotMappedError) { filter.parse_request_data(client_data, [expected_method_name]) }
   end
+
+  def test_sanitize_method_name
+    filter = RequestDataFiltering
+
+    assert_equal "get.test", filter.sanitize_method_name("GET /test ")
+    assert_equal "post.user.profile", filter.sanitize_method_name("POST /user/profile ")
+    assert_equal "put.items.123", filter.sanitize_method_name("PUT /items/123 ")
+  end
+
+  def test_extract_headers
+    filter = RequestDataFiltering
+
+    header_data1 = "Content-Type: application/json\r\nAccept: */*\r\nHost: localhost\r\nContent-Length: 29\r\n\r\n"
+    header_data2 = "Cache-Control: no-cache\r\nPragma: no-cache\r\nExpires: -1\r\n\r\n"
+
+    expected_headers1 = {
+      "Content-Type" => "application/json",
+      "Accept" => "*/*",
+      "Host" => "localhost",
+      "Content-Length" => "29"
+    }
+
+    expected_headers2 = {
+      "Cache-Control" => "no-cache",
+      "Pragma" => "no-cache",
+      "Expires" => "-1"
+    }
+
+    header_io1 = StringIO.new(header_data1)
+    header_io2 = StringIO.new(header_data2)
+
+    _, headers1 = filter.extract_headers(header_io1)
+    _, headers2 = filter.extract_headers(header_io2)
+
+    assert_equal expected_headers1, headers1
+    assert_equal expected_headers2, headers2
+  end
+
+  def test_sanitize_parameters
+    filter = RequestDataFiltering
+
+    assert_equal "parameter1", filter.sanitize_parameter_name("parameter1")
+    assert_equal "parameter1", filter.sanitize_parameter_name("parameter1&$^#%")
+    assert_equal "123value", filter.sanitize_parameter_value("123value")
+    assert_equal "123value", filter.sanitize_parameter_value("123value\n ")
+  end
 end
