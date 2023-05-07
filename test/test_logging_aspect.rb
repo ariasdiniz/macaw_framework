2ef9fcedd7a8772ffb77c52c62dd4e39745f1642
Aria Diniz <58334893+ariasdiniz@users.noreply.github.com>
2023-05-06 21:53:47 -0300

    Revert "Creating log sanitization to prevent log forging (#47)" (#48)
    This reverts commit f133737b96c5b30277b73e361d3a90d6586798ee.

--- test/test_logging_aspect.rb
+++ test/test_logging_aspect.rb
@@ -1,9 +1,8 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 
 require "logger"
 require_relative "test_helper"
 require_relative "../lib/macaw_framework/aspects/logging_aspect"
-require_relative "../lib/macaw_framework/data_filters/log_data_filter"
 
 class LoggingAspectTest < Minitest::Test
   include LoggingAspect
@@ -12,20 +11,13 @@ class LoggingAspectTest < Minitest::Test
     @logger = Logger.new($stdout)
   end
 
-  def test_logs_sanitized_input_and_output
+  def test_logs_input_and_output
     args = %w[arg1 arg2]
-    sensitive_data = "password=my_password"
-    input_data = "Input of my_endpoint: #{args} #{sensitive_data}"
-    output_data = "Output of my_endpoint: some response"
-
-    sanitized_input_data = LogDataFilter.sanitize_for_logging(input_data)
-    sanitized_output_data = LogDataFilter.sanitize_for_logging(output_data)
-
     @logger.stub(:info, nil) do |msg|
       if msg.is_a?(String) && msg.start_with?("Input of my_endpoint:")
-        assert_equal sanitized_input_data, msg
+        assert_match(/#{args}/, msg)
       elsif msg.is_a?(String) && msg.start_with?("Output of my_endpoint:")
-        assert_equal sanitized_output_data, msg
+        assert_equal "Output of my_endpoint: some response", msg
       end
     end
 
